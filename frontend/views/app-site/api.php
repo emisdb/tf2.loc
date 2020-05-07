1c34164931a5d1e8b2613475b167367fc70c662a
emisd <emisd@DESKTOP-CLVG4OB>
2020-05-07 18:29:57 +0300

    ссылка портала

--- frontend/views/app-site/api.php
+++ frontend/views/app-site/api.php
@@ -16,14 +16,15 @@ $this->registerCSS(<<<CSS
     background: #CFD6D3; /* Цвет фона */
 	text-align: right;
    }
-div.site-index h2 span{
+div.site-index h2 a{
 		margin: 0px 10px;
 	}
 CSS
 ); 
 ?>
 <div class="site-index">
-	<h2 class="headline text-info"><i class="fa fa-warning text-yellow"></i><span>Данные санкт-петербургского правительства</span></h2>
+	<h2 class="headline text-info"><i class="fa fa-warning text-yellow"></i>
+		<?= Html::a('Данные санкт-петербургского правительства','http://data.gov.spb.ru/developers/');?></h2>
 
      <div>
 		 <table border="1" class="result-data">
